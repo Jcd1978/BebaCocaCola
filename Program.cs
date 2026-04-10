4ed1634ebfa053af5b75bd5843f671da850041b3
Joelma <joelma.carmo.duarte@gmail.com>
2026-04-10 20:39:13 -0300

    Exercício BebaCocaCola concluído

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear ();
+Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write(@"
+     beba  coca  cola
+     babe        cola
+     beba  coca
+     babe  cola  caco
+     caco
+     cola
+          c l o a c a
+");
+Console.ResetColor();
+Console.WriteLine();
